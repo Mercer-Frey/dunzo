0a23ec87612ee69afca05d6451a89e8fd85c1549
Mercer-Frey <40339163+Mercer-Frey@users.noreply.github.com>
2025-12-11 05:07:42 +0200

    clean docker folder

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test-db')
+  async testDb() {
+    try {
+      const users = await this.prismaService.user.findMany();
+      console.log(users);
+      return { ok: true, count: users.length };
+    } catch (e) {
+      console.error('❌ TEST-DB ERROR:', e);
+      return { ok: false, error: String(e) };
+    }
   }
 }
