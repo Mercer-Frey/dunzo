b345b3c08a13127a3884f5b0564223f1d9bfc9a4
Mercer-Frey <40339163+Mercer-Frey@users.noreply.github.com>
2025-12-14 21:33:01 +0200

    enhanced architecture

--- backend/src/infrastructure/health/health.module.ts
+++ backend/src/infrastructure/health/health.module.ts
@@ -1,8 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { HealthController } from '@infrastructure/health/health.controller';
+import { TerminusModule } from '@nestjs/terminus';
+import { TerminusOptionsService } from '@infrastructure/health/terminus-options.check';
 
 @Global()
 @Module({
+  imports: [TerminusModule],
   controllers: [HealthController],
+  providers: [TerminusOptionsService],
 })
 export class HealthModule {}
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,27 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { APP_ENV } from '@env/env';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
       <router-outlet/>
-<!--      @if (ENV) {-->
-<!--          <p class="text-4xl"> hi prod</p>-->
-
-<!--      } @else {-->
-<!--          <p class="text-9xl">{{ hello()?.message }} / {{ env()?.env }} / {{ test()?.length }}</p>-->
-
-<!--      }-->
   `,
 })
 export class App {
-  // protected readonly ENV = APP_ENV.production;
-  // private readonly http = inject(HttpClient);
-  // protected readonly hello = toSignal(this.http.get<{ message: string }>(APP_ENV.apiUrl));
-  // protected readonly test = toSignal(this.http.get<[]>(APP_ENV.apiUrl + '/test-db'));
-  // protected readonly env = toSignal(this.http.get<{ env: string }>(APP_ENV.apiUrl + '/test-env'));
 }
