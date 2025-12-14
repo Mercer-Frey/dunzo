d7c95b14b065d8e1ce57ae6baa8eb116b9854f81
Mercer-Frey <40339163+Mercer-Frey@users.noreply.github.com>
2025-12-14 21:50:22 +0200

    adding swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from '@root/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,21 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('NestJS Clean Architecture API')
+      .setDescription('The NestJS Clean Architecture API description')
+      .setVersion('1.0')
+      .addTag('users')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('api/docs', app, document, {
+      jsonDocumentUrl: 'api/json',
+    });
+  }
 
   await app.listen(configService.get<number>(`${process.env.NODE_ENV}.APP_PORT`)!);
 }
